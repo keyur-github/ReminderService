956775ee2d14f28023713bccc0e63de19791817f
Keyur Panchal <keyur.np@somaiya.edu>
2023-08-13 18:00:57 +0530

    Added Notification Ticket Model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig'); 
-const { sendEmail } = require('./services/email-services');
+// const { sendEmail } = require('./services/email-services');
+const cron = require('node-cron');
 
 const setUpAndStartServer = () => {
     
@@ -13,13 +14,15 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started on Port ${PORT}`);
-
-        sendEmail(
-            '"Support" <support@admin.com>',
-            'keyur.np@somaiya.edu',
-            'This is a testing email',
-            'Hey How are you ? I hope you loved the support'
-        );
+        // sendEmail(
+        //     'support@admin.com',
+        //     'keyur.np@somaiya.edu',
+        //     'This is a testing email',
+        //     'Hey How are you ? I hope you loved the support'
+        // );
+        cron.schedule('*/2 * * * *', () => {
+            console.log("Running a task every 2 minutes");
+        });
     });
 
 }  
